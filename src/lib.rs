5ab9a68f88b2f3542ca62b0838c48090b72d981f
Edward Yang <edward.yang6771@gmail.com>
2016-11-28 13:19:26 -0600

    Setup for simd

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+#[cfg(feature = "simd-opts")]
+extern crate simd;
+
 mod point;
 mod vector;
 mod object;
 mod gravity;
 mod universe;
+
+#[cfg(feature = "simd-opts")]
+mod simd_opts;
+
 pub use point::Point;
 pub use vector::Vector;
 pub use object::Object;
---
+++ src/simd_opts.rs
@@ -0,0 +1,4 @@
+// we can safely assume that sse2 is present for all x86 at this point
+use simd::x86::sse2::f64x2;
+
+
