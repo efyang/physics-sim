2efa1ecc9edf9951583561f54f02339e9ee76fbb
Edward Yang <edward.yang6771@gmail.com>
2016-12-10 21:18:30 -0600

    Make empty universes work

--- src/universe.rs
+++ src/universe.rs
@@ -20,9 +20,11 @@ impl Universe {
     /// returns list of indices removed
     pub fn update_state_repeat(&mut self, time: f64, iterations: usize) -> Vec<usize> {
         let mut removed_indices = Vec::new();
-        let time_per_iter = time / iterations as f64;
-        for _ in 0..iterations {
-            removed_indices.extend(self.update_state(time_per_iter));
+        if self.objects.len() > 0 {
+            let time_per_iter = time / iterations as f64;
+            for _ in 0..iterations {
+                removed_indices.extend(self.update_state(time_per_iter));
+            }
         }
         removed_indices
     }
@@ -30,33 +32,35 @@ impl Universe {
     /// returns list of indices removed
     pub fn update_state(&mut self, time: f64) -> Vec<usize> {
         let mut removed_indices = Vec::new();
-        // check for and handle any collisions
-        for i in 0..self.objects.len() - 1 {
-            for j in (i + 1..self.objects.len()).rev() {
-                if self.objects[i].is_colliding(&self.objects[j]) {
-                    if self.objects[i].more_massive(&self.objects[j]) {
-                        let other = self.objects[j].clone();
-                        self.objects[i].absorb_other(&other);
-                        self.objects.remove(j);
-                        removed_indices.push(j);
-                    } else {
-                        let other = self.objects[i].clone();
-                        self.objects[j].absorb_other(&other);
-                        self.objects.remove(i);
-                        removed_indices.push(i);
+        if self.objects.len() > 0 {
+            // check for and handle any collisions
+            for i in 0..self.objects.len() - 1 {
+                for j in (i + 1..self.objects.len()).rev() {
+                    if self.objects[i].is_colliding(&self.objects[j]) {
+                        if self.objects[i].more_massive(&self.objects[j]) {
+                            let other = self.objects[j].clone();
+                            self.objects[i].absorb_other(&other);
+                            self.objects.remove(j);
+                            removed_indices.push(j);
+                        } else {
+                            let other = self.objects[i].clone();
+                            self.objects[j].absorb_other(&other);
+                            self.objects.remove(i);
+                            removed_indices.push(i);
+                        }
                     }
                 }
             }
-        }
-        // get all the forces and apply them
-        // run in O(n^2) time
-        for i in 0..self.objects.len() {
-            for j in (0..self.objects.len()).filter(|&j| j != i) {
-                let (f1, f2) = gravitational_forces(&self.objects[i], &self.objects[j]);
-                self.objects[i].add_force(f1);
-                self.objects[j].add_force(f2);
+            // get all the forces and apply them
+            // run in O(n^2) time
+            for i in 0..self.objects.len() {
+                for j in (0..self.objects.len()).filter(|&j| j != i) {
+                    let (f1, f2) = gravitational_forces(&self.objects[i], &self.objects[j]);
+                    self.objects[i].add_force(f1);
+                    self.objects[j].add_force(f2);
+                }
+                self.objects[i].update_state(time);
             }
-            self.objects[i].update_state(time);
         }
         removed_indices
     }
